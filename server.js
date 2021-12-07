69e17c74fde9cd42b1f742dffc418df2987c8d5c
Thomas Koelling <thomas.koelling@check24.de>
2021-12-07 21:45:01 +0100

    add super basic prisma PoC as database layer

---
+++ server.js
@@ -0,0 +1,17 @@
+
+import Prisma from '@prisma/client'
+const prisma = new Prisma.PrismaClient()
+
+// await prisma.user.create({data: {name:'thomas', email: 'thomas@thomas.de', password:'hau'}})
+// await prisma.user.delete({
+//     where: {
+//         email: "thomas@thomas.de"
+//     }
+// })
+const users = await prisma.user.findMany({
+    select: {
+        name: true,
+        password: true,
+    },
+})
+console.log('users', users)
