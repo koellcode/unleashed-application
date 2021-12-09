77afb95a1b666546db38222e5e0ba0d23706b662
Thomas Koelling <thomas.koelling@check24.de>
2021-12-09 11:54:28 +0100

    store passwords hashed into db to secure it a bit

--- src/server/services/user/index.js
+++ src/server/services/user/index.js
@@ -1,10 +1,12 @@
+import * as bcrypt from 'bcrypt';
 import validateUser from '../../../common/validation/user';
 
 export default (db) => {
   return {
-    register: (user) => {
+    register: async (user) => {
       validateUser(user);
-      return db.user.create({ data: user });
+      const hash = await bcrypt.hash(user.password, 4);
+      return db.user.create({ data: { ...user, password: hash } });
     },
     getUserByEmail: (email) =>
       db.user.findUnique({
