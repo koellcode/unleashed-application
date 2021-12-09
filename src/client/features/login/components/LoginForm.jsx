5fa1da58703f1c39c33341fac36ee6b783b5bafc
Thomas Koelling <thomas.koelling@check24.de>
2021-12-09 11:24:15 +0100

    add basic formulars

--- src/client/features/login/components/LoginForm.jsx
+++ src/client/features/login/components/LoginForm.jsx
@@ -1,5 +1,44 @@
+import { Link } from 'react-router-dom';
+
 function LoginForm() {
-  return <h2>Login</h2>;
+  return (
+    <>
+      <div className="mb-4">
+        <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="email">
+          E-Mail
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+          id="email"
+          type="text"
+          placeholder="email"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" type="button">
+          Log In
+        </button>
+        <Link
+          className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+          to="/signup"
+          replace
+        >
+          Signup ?
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default LoginForm;
--- src/client/features/login/components/SignupForm.jsx
+++ src/client/features/login/components/SignupForm.jsx
@@ -1,5 +1,55 @@
+import { Link } from 'react-router-dom';
+
 function SignupForm() {
-  return <h2>Signup</h2>;
+  return (
+    <>
+      <div className="mb-4">
+        <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="email">
+          E-Mail
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+          id="email"
+          type="text"
+          placeholder="email"
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="name">
+          Name
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+          id="name"
+          type="text"
+          placeholder="name"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" type="button">
+          Sign up
+        </button>
+        <Link
+          className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+          to="/login"
+          replace
+        >
+          Login ?
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default SignupForm;
